6d9fef81dfbfe3c80301462f54bc792460b0876b
Rishabh Bhatnagar <33839817+megaelitez@users.noreply.github.com>
2020-10-04 18:22:07 -0400

    Jenkins commit, fixes helloworld

---
+++ build/precache-manifest.68510aa8839dea87e3f57d36b8952035.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31722a2325c7df3eb21772a469dbad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1783e647548f565cf281",
+    "url": "/static/css/main.1ba3d64a.chunk.css"
+  },
+  {
+    "revision": "d8ecba3d3cc14d7d564d",
+    "url": "/static/js/2.93b7faf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.93b7faf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1783e647548f565cf281",
+    "url": "/static/js/main.2d217c63.chunk.js"
+  },
+  {
+    "revision": "e80f5324011a1274c1a3",
+    "url": "/static/js/runtime-main.938752d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/papyrus.systems/precache-manifest.6d64b83ef80a1a0b7bc363ae000434b0.js"
+  "/precache-manifest.68510aa8839dea87e3f57d36b8952035.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/papyrus.systems/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
